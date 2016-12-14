abd16142c183857b768aefcaf9fcff813cb365ff
JOSCodebusters <ritesh.kumarsingh65@gmail.com>
2016-12-13 23:43:19 -0500

    Add files via upload

---
+++ output.c
@@ -0,0 +1,23 @@
+#include "ns.h"
+
+extern union Nsipc nsipcbuf;
+
+void
+output(envid_t ns_envid)
+{
+	binaryname = "ns_output";
+
+	// LAB 6: Your code here:
+	// 	- read a packet from the network server
+	//	- send the packet to the device driver
+	
+	int r = 0;
+	
+	while(1){
+		r = sys_ipc_recv(&nsipcbuf);
+		
+		if((thisenv->env_ipc_from == ns_envid) && (thisenv->env_ipc_value == NSREQ_OUTPUT)){
+			while((r = sys_e1000_tx(nsipcbuf.pkt.jp_data, nsipcbuf.pkt.jp_len)) < 0);
+		}
+	}
+}
